f940451c1bbc0d16aa367ff9bdef6fc54021fe61
miguemunoz <miguemunoz@protonmail.ch>
2020-12-14 22:22:03 +0000

    [AoC_2015, 1st] Comments section changed.

--- 2015/01/1.py
+++ 2015/01/1.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-""" AoC2015, 1st day, 1st star """
+"""
+    AoC2015, 1st day, 1st star.
+    
+    https://adventofcode.com/2015/day/1
+"""
 
 with open('input.txt') as f:
     for line in f:
