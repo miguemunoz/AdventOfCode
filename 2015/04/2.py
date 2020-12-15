c895fc2b32d4610723af00cf2c5b844824361332
miguemunoz <miguemunoz@protonmail.ch>
2020-12-15 17:01:17 +0000

    [AoC_2015, 4th] Solution added.

---
+++ 2015/04/2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+    AoC2015, 4th day, 2nd star.
+    
+    https://adventofcode.com/2015/day/4
+"""
+ 
+import hashlib 
+
+with open('input.txt') as f:
+    for line in f:
+        cnt = 0
+        while True:
+            result = hashlib.md5(line.strip().encode() + str(cnt).encode()).hexdigest()
+            if result.startswith('0' * 6):
+                 print(cnt)
+                 exit(0)
+            cnt += 1
