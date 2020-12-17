24d09a0fc25fca1aec8579de4bf6c7bcf7d625f4
miguemunoz <miguemunoz@protonmail.ch>
2020-12-17 00:26:05 +0000

    [AoC_2020, 16th] Minos cosmetic issues.

--- 2020/16/2.py
+++ 2020/16/2.py
@@ -8,27 +8,28 @@
 
 import re
 
-rules = {}
-
-yours = nearby = []
-
-with open('input.txt') as f:
-
-    lines = f.readlines()
+def parser(filename):
     
-    # Just parsering...
-    for idx, line in enumerate(lines):
-        if re.search(r'^[\w\s]+:\s\d+-\d+\sor\s\d+-\d+$', line.strip()):
-            key, ranges = line.split(':')[0], line.split(':')[1]
-            value = set()
-            for r in ranges.split(' or '):
-                value = value.union(set(list(range(int(r.strip().split('-')[0]),int(r.strip().split('-')[1])+1))))
-            rules[key] = value
-        elif re.search(r'^your ticket:$', line.strip()):
-            yours = [int(x) for x in lines[idx+1].split(',')]
-        elif re.search(r'^nearby tickets:$', line.strip()):
-            for j in lines[idx+1:]:
-                nearby.append([int(x) for x in j.split(',')])
+    rules = {}
+    yours = nearby = []
+    
+    with open(filename) as f:
+        lines = f.readlines()
+        for idx, line in enumerate(lines):
+            if re.search(r'^[\w\s]+:\s\d+-\d+\sor\s\d+-\d+$', line.strip()):
+                key, ranges = line.split(':')[0], line.split(':')[1]
+                value = set()
+                for r in ranges.split(' or '):
+                    value = value.union(set(list(range(int(r.strip().split('-')[0]),int(r.strip().split('-')[1])+1))))
+                rules[key] = value
+            elif re.search(r'^your ticket:$', line.strip()):
+                yours = [int(x) for x in lines[idx+1].split(',')]
+            elif re.search(r'^nearby tickets:$', line.strip()):
+                for j in lines[idx+1:]:
+                    nearby.append([int(x) for x in j.split(',')])
+    return rules, yours, nearby
+
+def remove_invalids(rules, nearby):
     
     valid = set()
     
@@ -40,33 +41,42 @@ with open('input.txt') as f:
             if field not in valid:
                 nearby.remove(ticket)
     
-    tickets = nearby + [yours]
-    
-    solution = {}
-    
-    for field in rules.keys():
-        solution[field] = []
-        for idx, column in enumerate(list(zip(*tickets))):
-            if set(column).issubset(rules[field]):
-                solution[field].append(idx)
-    
-    changes = True
-    
-    checked = set()
+    return nearby
 
-    while changes:
-        changes = False
-        for field in rules.keys():
-            if len(solution[field]) == 1 and field not in checked:
-                checked.add(field)
-                for s in solution.values():
-                    if len(s) > 1:
-                        s.remove(solution[field][0])
-                        changes = True
-    
-    r = 1
-    for field in solution.keys():
-            if re.search(r'^departure', field):
-                print(solution[field][0])
-                r *= yours[solution[field][0]]
-    print(r)
+# Parse the input file...
+
+rules, yours, nearby = parser('input.txt')
+
+# Remove invalids...
+
+nearby = remove_invalids(rules, nearby)
+
+# Calculate all the candidates solution...
+
+tickets = nearby + [yours]
+
+solution = {}
+
+for field in rules.keys():
+    solution[field] = []
+    for idx, column in enumerate(list(zip(*tickets))):
+        if set(column).issubset(rules[field]):
+            solution[field].append(idx)
+
+# Discards candidates until gets the solution...
+
+already_checked = set()
+
+while len(already_checked) < len(rules.keys()):
+    for field in rules.keys():
+        if len(solution[field]) == 1 and field not in already_checked:
+            already_checked.add(field)
+            for s in solution.values():
+                if len(s) > 1:
+                    s.remove(solution[field][0])
+
+result = 1
+for field in solution.keys():
+    if re.search(r'^departure', field):
+        result *= yours[solution[field].pop()]
+print(result)
