6ba3a2a4351f84fa477075f52df5ed38c84d5794
miguemunoz <miguemunoz@protonmail.ch>
2020-12-20 22:56:32 +0000

    [AoC_2020, 3th] Solution simplified

--- 2020/03.py
+++ 2020/03.py
@@ -6,15 +6,19 @@
 
 from functools import reduce
 
-def aoc2020d3s1(filename):
+def aoc2020d3(filename, first_star):
     
-    with open(filename) as f:
-        
-        trees = 0
-        
-        geomap =  [list(x.strip()) * 3300 for x in f.readlines()]
-        
+    slopes = [(3,1)] if first_star else [(3,1), (1,1),(5,1),(7,1),(1,2)]
+    
+    solution = []
+    
+    geomap =  [list(x.strip()) * 3300 for x in open(filename)]
+    
+    for i,j in slopes:
+    
         x, y = 0, 0 # start position
+        
+        trees = 0 # number of trees found for the slope
         
         while y < len(geomap):
             if geomap[y][x] == '.':
@@ -22,41 +26,13 @@ def aoc2020d3s1(filename):
             elif geomap[y][x] == '#':
                 geomap[y][x] = 'X'
                 trees += 1
-            x += 3
-            y += 1
+            x += i
+            y += j
         
-        return trees
-
-def aoc2020d3s2(filename):
+        solution.append(trees)
     
-    slopes = [(3,1), (1,1),(5,1),(7,1),(1,2)]
-    
-    solution = []
-    
-    with open(filename) as f:
-        
-        geomap =  [list(x.strip()) * 3300 for x in f.readlines()]
-        
-        for i,j in slopes:
-        
-            x, y = 0, 0 # start position
-            
-            trees = 0 # number of trees found for the slope
-            
-            while y < len(geomap):
-                if geomap[y][x] == '.':
-                    geomap[y][x] = 'O'
-                elif geomap[y][x] == '#':
-                    geomap[y][x] = 'X'
-                    trees += 1
-                x += i
-                y += j
-            
-            solution.append(trees)
-        
-        return reduce((lambda x, y: x * y), solution)
+    return reduce((lambda x, y: x * y), solution)
 
 if __name__ == "__main__":
-    print(f'aoc2020d3s1: {aoc2020d3s1("input.txt")}')
-    print(f'aoc2020d3s2: {aoc2020d3s2("input.txt")}')
-
+    print(f'aoc2020d3s1: {aoc2020d3("input.txt", True)}')
+    print(f'aoc2020d3s2: {aoc2020d3("input.txt", False)}')
