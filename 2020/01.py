f8ff35f4bd408797b121e0a3da5747767edd3c86
miguemunoz <miguemunoz@protonmail.ch>
2020-12-20 22:40:02 +0000

    [AoC_2020, 2nd] Solution simplified

--- 2020/01.py
+++ 2020/01.py
@@ -4,19 +4,14 @@
     AoC2020, 1st day ( https://adventofcode.com/2020/day/1 )
 """
 
-def aoc2020d1s1(filename):
-    
+def aoc2020d1(filename, first_star):
     with open(filename) as f:
         expenses = [int(expense) for expense in f]
-        return [x*y for x in expenses for y in expenses if x+y == 2020][0]
-
-def aoc2020d1s2(filename):
-    
-    with open(filename) as f:
-        expenses = [int(expense) for expense in f]
-        return [x*y*z for x in expenses for y in expenses for z in expenses if x+y+z == 2020][0]
+        if first_star:
+            return [x*y for x in expenses for y in expenses if x+y == 2020][0]
+        else:
+            return [x*y*z for x in expenses for y in expenses for z in expenses if x+y+z == 2020][0]
 
 if __name__ == "__main__":
-    print(f'aoc2020d1s1: {aoc2020d1s1("input.txt")}')
-    print(f'aoc2020d1s2: {aoc2020d1s2("input.txt")}')
-
+    print(f'aoc2020d1s1: {aoc2020d1("input.txt", True)}')
+    print(f'aoc2020d1s2: {aoc2020d1("input.txt", False)}')
--- 2020/02.py
+++ 2020/02.py
@@ -4,7 +4,7 @@
     AoC2020, 2nd day ( https://adventofcode.com/2020/day/2 )
 """
 
-def aoc2020d2s1(filename):
+def aoc2020d2(filename, first_star):
     
     cnt = 0x0
     
@@ -14,25 +14,13 @@ def aoc2020d2s1(filename):
         idx = policy.split()[0].split('-')
         letter = policy.split()[1]
         
-        cnt += 1 if password.count(letter) in range(int(idx[0]),int(idx[1])+1) else 0
-    
-    return cnt
-
-def aoc2020d2s2(filename):
-    
-    cnt = 0x0
-    
-    for line in open(filename):
-        
-        policy, password = line.split(':')
-        idx = policy.split()[0].split('-')
-        letter = policy.split()[1]
-        
-        cnt += 1 if (password[int(idx[0])] == letter) != (password[int(idx[1])] == letter) else 0
+        if first_star:
+            cnt += 1 if password.count(letter) in range(int(idx[0]),int(idx[1])+1) else 0
+        else:
+            cnt += 1 if (password[int(idx[0])] == letter) != (password[int(idx[1])] == letter) else 0
     
     return cnt
 
 if __name__ == "__main__":
-    print(f'aoc2020d2s1: {aoc2020d2s1("input.txt")}')
-    print(f'aoc2020d2s2: {aoc2020d2s2("input.txt")}')
-
+    print(f'aoc2020d2s1: {aoc2020d2("input.txt", True)}')
+    print(f'aoc2020d2s2: {aoc2020d2("input.txt", False)}')
