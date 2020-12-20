64ede2ade16c738e94fa9deea7516637b5bc7085
miguemunoz <miguemunoz@protonmail.ch>
2020-12-20 06:14:45 +0000

    [AoC 2020, 1st] Again, the code changes...

--- 2020/01.py
+++ 2020/01.py
@@ -1,4 +1,23 @@
+#!/usr/bin/env python3
 
-with open('input.txt') as f:
-    expenses = [int(x) for x in f.readlines()]
-    print([x*y for x in expenses for y in expenses if x+y == 2020][0])
+"""
+    AoC2020, 1st day.
+    
+    https://adventofcode.com/2015/day/1
+"""
+
+def aoc2020d1s1(filename):
+    
+    with open(filename) as f:
+        expenses = [int(expense) for expense in f]
+        return [x*y for x in expenses for y in expenses if x+y == 2020][0]
+
+def aoc2020d1s2(filename):
+    
+    with open(filename) as f:
+        expenses = [int(expense) for expense in f]
+        return [x*y*z for x in expenses for y in expenses for z in expenses if x+y+z == 2020][0]
+
+if __name__ == "__main__":
+    print(f'aoc2020d1s1: {aoc2020d1s1("input.txt")}')
+    print(f'aoc2020d1s2: {aoc2020d1s2("input.txt")}')
